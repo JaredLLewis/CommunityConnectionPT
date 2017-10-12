df89cc6fe16df2f222dd4539ba6ae525b5e61282
Finton38 <32285866+Finton38@users.noreply.github.com>
2017-10-11 23:35:27 -0400

    Add project files.

---
+++ CommunityConnectionPT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommunityConnectionPT.MvcApplication" Language="C#" %>
---
+++ CommunityConnectionPT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityConnectionPT.Startup))]
+namespace CommunityConnectionPT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
